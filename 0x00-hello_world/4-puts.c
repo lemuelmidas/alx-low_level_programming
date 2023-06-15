65dcb53d427cb5ae0a22e7d7b8dc14697234af49
lemuel <xerxes50@yahoo.com>
2023-06-15 14:29:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * a program which main prints: "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * puzzle, followed by a new line.
  * it should return 0
  */
 
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
