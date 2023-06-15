a2c92c5a53785463d39e50ab66f5494c6a24320d
lemuel <xerxes50@yahoo.com>
2023-06-15 10:28:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,17 @@
 
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+ int main(void)
+ {
+     
   puts("Programming is like a multilingual puzzle");
   return 0;
-  
-}
+
+ }
+ 
