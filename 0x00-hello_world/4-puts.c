c1a8de31a5c841a6c73d7fd34ed5ed7010b1c71d
lemuel <xerxes50@yahoo.com>
2023-06-15 11:03:13 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,15 @@
 
 #include <stdio.h>
 
-/**
- * main - Entry point
+/*
+ * a program which main prints: "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
  */
- 
- int main(void)
- {
-     
-  puts("Programming is like a multilingual puzzle");
-  return 0;
 
- }
- 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
