7f9dcf4efc72e937d201348cf9a207cfac93ee74
lemuel <xerxes50@yahoo.com>
2023-06-15 14:11:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 #include <stdio.h>
 
-/*
+/**
  * a program which main prints: "Programming is like building a multilingual
  *                puzzle, followed by a new line.
- *
+ * it should return 0
  */
 
 int main(void)
