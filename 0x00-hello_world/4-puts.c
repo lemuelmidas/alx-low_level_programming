32e5a03e29a196e1d7a3f5c7496b39803bae58d8
lemuel <xerxes50@yahoo.com>
2023-06-17 14:59:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,14 @@
-/*
- * File title: 4-puts.c
- * Author: Emamniuel Justine
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(void) 
 {
-
-      puts("\"Programming is like building a multilingual puzzle");
-      
-      return (0);
-      
+        puts("Programming is like building a multilingual puzzle");
+	
+	return (0);
 }
