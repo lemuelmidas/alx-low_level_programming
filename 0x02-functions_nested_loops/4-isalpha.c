bd24a46f7d314a96004a3ce58de6f66a1a00cb8d
lemuel <xerxes50@yahoo.com>
2023-06-20 06:48:56 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main: check the code
+ * Return: 1 or 0 in otherwise.
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int letter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				letter = 1;
+		}
+	}
+	return (letter);
+}
