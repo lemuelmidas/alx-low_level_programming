ff9d1eb89adc271bf9da376f6cd82bc9acd53617
lemuelmidas <xerxes50@yahoo.com>
2023-06-21 03:42:31 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,11 @@
 
 /**
  * add - check the code
- *
+ * @i: an integer i
+ * @j: an integer j
  * Return: result of i + j
  */
 int add(int i, int j)
 {
-
-        return (i + j);
-
+	return (i + j);
 }
