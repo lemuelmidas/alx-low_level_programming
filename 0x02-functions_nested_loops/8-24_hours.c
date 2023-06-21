193bcb9dbf2a6a6e4fd84ea33968072e94c88200
lemuelmidas <xerxes50@yahoo.com>
2023-06-21 05:46:51 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,29 +2,24 @@
 
 /**
  * jack_bauer - check main content.
- * Description: function prints every minute in one day
  * Return: Nothing
  */
 void jack_bauer(void)
 {
+int i, j;
 
-        int i, j;
+for (i = 0; i < 24; i++)
+{
 
-	for (i = 0; i < 24; i++)
+	for (j = 0; j < 60; j++)
 	{
-
-	        for (j = 0; j < 60; j++)
-		{
-		
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			
-		}
-
+	_putchar((i / 10) + '0');
+	_putchar((i % 10) + '0');
+	_putchar(':');
+	_putchar((j / 10) + '0');
+	_putchar((j % 10) + '0');
+	_putchar('\n');
 	}
+}
 
 }
