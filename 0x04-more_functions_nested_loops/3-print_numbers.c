860828f7e2cb50bc42dd9db50dcef808ab1817e8
lemuelmidas <xerxes50@yahoo.com>
2023-06-23 07:04:51 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code,
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int p;
+
+	for (p=0; p <= '9'; p++)
+        {
+	 _putchar(p);
+	}
+	_putchar('\n');
+}
