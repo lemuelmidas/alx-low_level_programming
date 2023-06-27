f734b08cd99ad710524fd4944d03a93d26a132cc
lemuelmidas <xerxes50@yahoo.com>
2023-06-27 12:57:14 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,3 +9,14 @@
  * Return: Nothing
  */
 void print_array(int *a, int n)
+{
+	int w;
+
+	for (w = 0; w < n; w++)
+	{
+		printf("%d", a[w]);
+		if (w < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
