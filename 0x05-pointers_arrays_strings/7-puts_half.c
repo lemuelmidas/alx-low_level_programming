6f2ac414380c71d6584cb58bba1c1d6be199a4d8
lemuelmidas <xerxes50@yahoo.com>
2023-06-27 12:51:12 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,3 +6,20 @@
  * Return: Nothing
  */
 void puts_half(char *str)
+{
+	int length = 0, a, k;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		k = length / 2;
+
+	else
+		k = (length + 1) / 2;
+
+	for (a = k; a < length; a++)
+		_putchar(str[a]);
+
+	_putchar('\n');
+}
