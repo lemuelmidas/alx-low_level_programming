bcd5acec7730f60234bc48622743e6ff69233731
lemuelmidas <xerxes50@yahoo.com>
2023-06-27 12:43:13 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,3 +7,15 @@
  */
 void puts2(char *str)
 {
+        int length = 0, a = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	length -= 1;
+
+	for (a = 0; a <= length; a += 2)
+		_putchar(str[a]);
+
+	_putchar('\n');
+}
