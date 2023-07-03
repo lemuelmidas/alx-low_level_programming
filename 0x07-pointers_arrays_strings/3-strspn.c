20c35e1d6d83e39a54ceb313be3958b8cff97eb2
lemuelmidas <xerxes50@yahoo.com>
2023-07-03 13:06:02 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+
+/**
+ * _strspn - to get the length of a prefix substring.
+ * @s: input string
+ * @accept: input character with to locate into string s
+ * Return: returns pointer to c position
+ */
+unsigned int _strspn(char *s, char *accept)
