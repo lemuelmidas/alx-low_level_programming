47586f6adf58d4cb75484a5eb30a7a1b4296145e
lemuelmidas <xerxes50@yahoo.com>
2023-07-06 10:40:12 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,9 @@
 #include "main.h"
-int is_divisible(int dsor, int dend);
+int is_divisible(int dividend, int divisor);
 
 /**
  * is_prime_number - to check if a number is prime.
+ * prime numbers when divided by 2 will give a remainder of 1
  * @n: an input integer
  * Return: 1 when n is prime or  0 if not
  */
@@ -21,17 +22,17 @@ int is_prime_number(int n)
 
 /**
  * is_divisible - to check if num is divisible
- * @dsor: the divisor
- * @dend: the dividend
+ * @dividend: the dividend
+ * @divisor: the divisor
  * Return: 1 if num is divisible or 0 if numis not divisible
  */
-int is_divisible(int dsor, int dend)
+int is_divisible(int dividend, int divisor)
 {
-	if (dsor % dend == 0)
+	if (dividend % divisor == 0)
 		return (0);
 
-	if (dend == dsor / 2)
+	if (divisor == dividend / 2)
 		return (1);
 
-	return (is_divisible(dsor, dend + 1));
+	return (is_divisible(dividend, divisor + 1));
 }
