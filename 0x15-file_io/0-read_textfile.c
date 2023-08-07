a225128041a70573c87ecfe4c412f9f37518d297
lemuelmidas <xerxes50@yahoo.com>
2023-08-07 08:21:55 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read a text file and print
+ * to the POSIX STDOUT
+ * @filename: The filename to open
+ * @letters: The number of letters to read and print
+ * Return: The count of letters read and printed. If failed, 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int open_file, read_file, write_file;
+	char *memory;
+
+	if (filename == NULL)
+		return (0);
+
+	memory = malloc(sizeof(char) * letters);
+	if (memory == NULL)
+		return (0);
+
+	open_file = open(filename, O_RDONLY);
+	if (open_file < 0)
+	{
+		free(memory);
+		return (0);
+	}
+
+	read_file = read(open_file, memory, letters);
+	if (read_file < 0)
+	{
+		free(memory);
+		return (0);
+	}
+
+	write_file = write(STDOUT_FILENO, memory, read_file);
+	free(memory);
+	close(open_file);
+
+	if (write_file < 0)
+		return (0);
+	return ((ssize_t)write_file);
+}
